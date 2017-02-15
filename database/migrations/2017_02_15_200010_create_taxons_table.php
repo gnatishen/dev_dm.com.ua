eed79db6c8b651540c5da23f8d918cd768d17b06
Gnatishen Yuriy <gnatishen@gmail.com>
2017-02-15 22:13:02 +0200

    laravel and migrations

---
+++ database/migrations/2017_02_15_200010_create_taxons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaxonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taxons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('vocab_id');
+            $table->string('url_latin');
+            $table->string('product_ids')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taxons');
+    }
+}
